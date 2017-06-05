820a7153fa220007fa3676fecb8ab1190d90e070
Levi Stenton <levistenton@gmail.com>
2017-06-05 13:36:56 +1000

    -Added Animation Controller, can't figure out why the character isn't moving

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    public Animator anim;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = player.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        WarpAnim();
+
+        if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            anim.SetBool("walking", true);
+        }            
+        else anim.SetBool("walking", false);
+    }
+
+    public void WarpAnim()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.gameObject.tag == "warpActivate")
+                {
+                    anim.SetBool("warpActivate1", true);
+                }
+                else anim.SetBool("warpActivate1", false);
+
+                if (hit.transform.gameObject.tag == "warpActivate1")
+                {
+                    anim.SetBool("warpActivate2", true);
+                }
+                else anim.SetBool("warpActivate2", false);
+
+                if (hit.transform.gameObject.tag == "warpActivate2")
+                {
+                    anim.SetBool("warpActivate3", true);
+                }
+                else anim.SetBool("warpActivate3", false);
+
+                if (hit.transform.gameObject.tag == "warpActivate3")
+                {
+                    anim.SetBool("warpActivate4", true);
+                }
+                else anim.SetBool("warpActivate4", false);
+            }            
+        }        
+    }
+}
